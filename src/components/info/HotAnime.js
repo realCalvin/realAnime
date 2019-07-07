46366dfb921ce13338d4a89e4349e70c64d4b43f
Calvin Ly <lycalvin99@gmail.com>
2019-07-06 19:02:48 -0700

    added filter functionality in hot anime

--- src/components/info/HotAnime.js
+++ src/components/info/HotAnime.js
@@ -38,6 +38,49 @@ class HotAnime extends Component {
         console.log(this.state.anime)
     }
 
+    handleFilter = (id) => {
+        let list = null;
+        this.setState({
+            animeList: null,
+        })
+        axios.get("https://api.jikan.moe/v3/top/anime/1/" + id)
+            .then((response) => {
+                let animes = response.data.top;
+                var topAnime = animes.map(anime => {
+                    return (
+                        <div key={anime.mal_id} className="row hot-anime">
+                            <div className="col-md-5 hot-anime-pic">
+                                <img className="hot-img" src={anime.image_url} alt={anime.title} onClick={() => { this.handleModal(anime.mal_id) }} data-toggle="modal" data-target="#subHotAnimeInfoModal"></img>
+                            </div>
+                            <div className="col-md-7 hot-anime-info">
+                                <ul className="hot-list">
+                                    <li><h4 onClick={() => { this.handleModal(anime.mal_id) }} data-toggle="modal" data-target="#subHotAnimeInfoModal">{anime.title}</h4></li>
+                                    <li className="indent">Rank: {anime.rank}</li>
+                                    <li className="indent">Date: {anime.start_date} - {anime.end_date}</li>
+                                    <li className="indent">Episodes: {anime.episodes ? anime.episodes : "N/A"}</li>
+                                    <li className="indent">Score: {anime.score}</li>
+                                </ul>
+                                <div className="hot-anime-btn">
+                                    <button className="btn btn-primary" onClick={() => { this.handleModal(anime.mal_id) }} data-toggle="modal" data-target="#subHotAnimeInfoModal">More Information</button>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                });
+                list = topAnime;
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+            .then(() => {
+                if (this.state.animeList == null) {
+                    this.setState({
+                        animeList: list,
+                    })
+                }
+            })
+    }
+
     componentDidMount() {
         let list = null;
         axios.get("https://api.jikan.moe/v3/top/anime/1/tv")
@@ -96,10 +139,10 @@ class HotAnime extends Component {
                             </div>
                             <div className="modal-body">
                                 <Row>
-                                    <button className="btn btn-dark btn-sm btn-filter" id="">All Animes</button>
-                                    <button className="btn btn-dark btn-sm btn-filter" id="airing">Top Airing</button>
-                                    <button className="btn btn-dark btn-sm btn-filter" id="movie">Top Movies</button>
-                                    <button className="btn btn-dark btn-sm btn-filter" id="upcoming">Top Upcoming</button>
+                                    <button className="btn btn-dark btn-sm btn-filter" onClick={() => { this.handleFilter("") }}>All Animes</button>
+                                    <button className="btn btn-dark btn-sm btn-filter" onClick={() => { this.handleFilter("airing") }}>Top Airing</button>
+                                    <button className="btn btn-dark btn-sm btn-filter" onClick={() => { this.handleFilter("movie") }}>Top Movies</button>
+                                    <button className="btn btn-dark btn-sm btn-filter" onClick={() => { this.handleFilter("upcoming") }}>Top Upcoming</button>
                                 </Row>
                                 <Row>
                                     <div className="well anime-synopsis">
