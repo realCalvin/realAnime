86e207167802e5e1338d330c0650af4f12811803
Calvin Ly <lycalvin99@gmail.com>
2019-07-08 19:21:10 -0700

    Updates

---
+++ precache-manifest.bd6299f68cb68c63a6de7f059cc9e8fa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29aaa8c11c47b2dc304c9e32b0400314",
+    "url": "/anime/index.html"
+  },
+  {
+    "revision": "97be0a0e471d7550a37e",
+    "url": "/anime/static/css/main.19136527.chunk.css"
+  },
+  {
+    "revision": "71bb872dfd5655856c1c",
+    "url": "/anime/static/js/2.b7bc243c.chunk.js"
+  },
+  {
+    "revision": "97be0a0e471d7550a37e",
+    "url": "/anime/static/js/main.066ebd23.chunk.js"
+  },
+  {
+    "revision": "59cc1b31dbb0ddc994fc",
+    "url": "/anime/static/js/runtime~main.839a7f3c.js"
+  },
+  {
+    "revision": "1a6dc16baeed5eaa188112443edf22e9",
+    "url": "/anime/static/media/background.1a6dc16b.mp4"
+  },
+  {
+    "revision": "201f2e5d9c22d306990ed615385964b6",
+    "url": "/anime/static/media/kiminonawa.201f2e5d.mp3"
+  },
+  {
+    "revision": "0e5719ee4ef83147855a2c9753d6c262",
+    "url": "/anime/static/media/logo.0e5719ee.png"
+  },
+  {
+    "revision": "b6681619dcc616623083589aa826d2be",
+    "url": "/anime/static/media/user.b6681619.png"
+  }
+]);
